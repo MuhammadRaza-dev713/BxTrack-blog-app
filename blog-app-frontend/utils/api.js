3d3b1bd03eecf782bc724b25741e81360fe0d8a3
MuhammadRaza-dev713 <163148457+MuhammadRaza-dev713@users.noreply.github.com>
2025-11-12 19:10:13 +0500

    add frontend

---
+++ blog-app-frontend/utils/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const baseURL = process.env.NEXT_PUBLIC_API_BASE || "http://localhost:5000/api";
+
+const instance = axios.create({
+  baseURL,
+  headers: { "Content-Type": "application/json" }
+});
+
+// Optionally add request interceptor to attach token automatically
+instance.interceptors.request.use((config) => {
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("token");
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default instance;
